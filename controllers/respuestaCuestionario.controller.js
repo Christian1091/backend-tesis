6aeb072dac4ae54820d13d389ea71382f9283120
¨Christian <christianespinoza935@gmail.com>
2021-11-08 13:41:41 -0500

    Ver Estadisticas

--- controllers/respuestaCuestionario.controller.js
+++ controllers/respuestaCuestionario.controller.js
@@ -1,15 +1,16 @@
 const { response } = require('express');
+const { stringify } = require('uuid');
 
 const respuestaCuestionario = require('../models/respuestaCuestionario');
 
-const crearRespuestaCuestionario = async ( req, res = response ) => {
+const crearRespuestaCuestionario = async(req, res = response) => {
 
     let body = req.body;
     //console.log(body);
 
     try {
 
-        let respuesta = new respuestaCuestionario ({
+        let respuesta = new respuestaCuestionario({
             nombreParticipante: body.nombreParticipante,
             correoParticipante: body.correoParticipante,
             institucionParticipante: body.institucionParticipante,
@@ -26,27 +27,40 @@ const crearRespuestaCuestionario = async ( req, res = response ) => {
         const respuestaUsuarioCuestionarioDB = await respuesta.save();
 
         res.json({
-            ok:true,
+            ok: true,
             //msg: 'Guardando respuesta ususario',
             respuestaCuestionario: respuestaUsuarioCuestionarioDB
         })
-        
+
     } catch (error) {
         console.log(error)
         res.status(500).json({
             ok: false,
             msg: 'Hable con el administrador'
         })
-        
+
     }
 }
 
-// Este get es para visualizar publicamente
-const getRespuestaUsuario = async ( req, res = response ) => {
+const getRespuestasCuestionariosTodo = async(req, res = response) => {
 
-    const rtUsuarioid = req.params.id;     
+    //const cuestionarioId = req.params.id;
+    //console.log(cuestionarioId)
+    //const respuestaCuestionarioTodo = await respuestaCuestionario.find(); //.populate('hospital', 'nombre img')
+    const respuestaCuestionarioTodo = await respuestaCuestionario.find({ "listRespuestasUsuario.idPregunta": 1 });
+    res.json({
+        ok: true,
+        //msg: 'Funciona!'
+        respuestaCuestionarioTodo
+    })
+}
+
+// Este get es para visualizar publicamente
+const getRespuestaUsuario = async(req, res = response) => {
+
+    const rtUsuarioid = req.params.id;
     //console.log(rtUsuarioid)
-    const respuestaUsuario = await respuestaCuestionario.findById( rtUsuarioid );
+    const respuestaUsuario = await respuestaCuestionario.findById(rtUsuarioid);
     res.json({
         ok: true,
         //msg: 'Ver respuestas'
@@ -55,13 +69,12 @@ const getRespuestaUsuario = async ( req, res = response ) => {
 }
 
 // Este get es para visualizar los cuestionarios creados por el usuario ya autenticado en los cards
-const getRespuestaByIdCuestionario = async ( req, res = response ) => {
+const getRespuestaByIdCuestionario = async(req, res = response) => {
 
     const id = req.params.id;
     //const uid = req.uid;    
     //console.log(uid)
-    const respuestaByIdCuestionario = await respuestaCuestionario.find({cuestionarioId: id})//.populate('usuario', 'nombre')
-                                        ;//.populate('hospital', 'nombre img')
+    const respuestaByIdCuestionario = await respuestaCuestionario.find({ cuestionarioId: id });
     res.json({
         ok: true,
         //msg: 'Estadisticas'
@@ -69,31 +82,51 @@ const getRespuestaByIdCuestionario = async ( req, res = response ) => {
     })
 }
 
-const borrarRespuestaUsuario = async( req, res = response ) => {
+const getRespuestaByIdCuestionarioEstadisticas = async(req, res = response) => {
 
-    
+    const id = req.params.id;
+    //const uid = req.uid;    
+    //console.log(uid)
+    const respuestaByIdCuestionario = await (await respuestaCuestionario.find({ cuestionarioId: id })).forEach(function(record) {
+        console.log("Name: " + JSON.stringify(record.nombreParticipante));
+        console.log("Puntos: " + JSON.stringify(record.puntosTotales));
+    });
+    //const respuestaByIdCuestionario = await respuestaCuestionario.aggregate([{
+    //     $match: { cuestionarioId: id }
+    //} //, { $group: { _id: { puntaje: "$listRespuestasUsuario.tituloPregunta" } } }
+    //]);
+    res.json({
+        ok: true,
+        //msg: 'Estadisticas'
+        respuestaByIdCuestionario
+    })
+}
+
+const borrarRespuestaUsuario = async(req, res = response) => {
+
+
     const id = req.params.id;
 
     try {
-       
-        const respuestaUsuario = await respuestaCuestionario.findById( id );
 
-        if( !respuestaUsuario ) {
+        const respuestaUsuario = await respuestaCuestionario.findById(id);
+
+        if (!respuestaUsuario) {
             return res.status(404).json({
                 ok: false,
                 msg: 'Respuesta Usuario no encontrado por el id'
             });
         }
 
-        await respuestaCuestionario.findByIdAndDelete( id );
+        await respuestaCuestionario.findByIdAndDelete(id);
 
-       res.json({
-           ok: true,
-           msg: 'Respuesta Usuario eliminado'
-           
-       });
+        res.json({
+            ok: true,
+            msg: 'Respuesta Usuario eliminado'
 
-    }catch(error){
+        });
+
+    } catch (error) {
         console.log(error);
         res.status(500).json({
             ok: false,
@@ -107,5 +140,7 @@ module.exports = {
     crearRespuestaCuestionario,
     getRespuestaUsuario,
     getRespuestaByIdCuestionario,
+    getRespuestasCuestionariosTodo,
+    getRespuestaByIdCuestionarioEstadisticas,
     borrarRespuestaUsuario
 }
--- routes/respuestaCuestionario.js
+++ routes/respuestaCuestionario.js
@@ -2,24 +2,35 @@
  * respuestaCuestionarios
  * /api/respuestaCuestionarios
  */
- const { Router } = require('express');
- const { check } = require('express-validator');
- const { crearRespuestaCuestionario, getRespuestaUsuario, getRespuestaByIdCuestionario, borrarRespuestaUsuario } = require('../controllers/respuestaCuestionario.controller');
- const { validarCampos } = require('../middlewares/validar-campos');
- 
- const { validarJWT } = require('../middlewares/validar-jwt');
- 
- const router = Router();
- 
+const { Router } = require('express');
+const { check } = require('express-validator');
+const {
+    crearRespuestaCuestionario,
+    getRespuestaUsuario,
+    getRespuestaByIdCuestionario,
+    getRespuestasCuestionariosTodo,
+    getRespuestaByIdCuestionarioEstadisticas,
+    borrarRespuestaUsuario
+} = require('../controllers/respuestaCuestionario.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
- router.get( '/:id', getRespuestaUsuario);
+const { validarJWT } = require('../middlewares/validar-jwt');
 
- router.get( '/estadisticas/:id', validarJWT, getRespuestaByIdCuestionario);
+const router = Router();
 
- router.post( '/', crearRespuestaCuestionario );
- 
- router.delete( '/:id',validarJWT, borrarRespuestaUsuario);
- 
- 
- // Exportamos el router
- module.exports = router;
+
+router.get('/:id', getRespuestaUsuario);
+
+router.get('/', getRespuestasCuestionariosTodo);
+
+router.get('/estadisticas/:id', validarJWT, getRespuestaByIdCuestionario);
+
+router.get('/estadisticasPreguntas/:id', validarJWT, getRespuestaByIdCuestionarioEstadisticas);
+
+router.post('/', crearRespuestaCuestionario);
+
+router.delete('/:id', validarJWT, borrarRespuestaUsuario);
+
+
+// Exportamos el router
+module.exports = router;
