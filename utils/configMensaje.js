ca17b24fcb0ed66235ae32f2993a9b5be0956cb6
Elizabeth <puyaguaric@est.ups.edu.ec>
2022-01-31 13:56:33 -0500

    EmailGoogle

--- utils/configMensaje.js
+++ utils/configMensaje.js
@@ -1,17 +1,32 @@
 const nodemailer = require('nodemailer');
 
-const sendMails = (mails,url,cuestionario) => {
+const {google}= require('googleapis');
+const CLIENT_ID = "54531413292-5vj3u3goqcg9qvcfvun9iptntktg7fkb.apps.googleusercontent.com"; 
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "1//04U-0F_FSOXJsCgYIARAAGAQSNwF-L9Ir2J3KG3oXfo3vNlgWAkqo8lqINpfpLe0RHNf3L7fhnJ-elpBUEUJ2DUOm2Jh3fVNXeTY"; 
+const oauth_cliente = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI);
+oauth_cliente.setCredentials({
+    refresh_token: REFRESH_TOKEN
+
+})
+const sendMails = async (mails,url,cuestionario) => {
+    const accessToken = await oauth_cliente.getAccessToken()
     var transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
         secure: false,
-        ignoreTLS:false,
+        service: 'gmail',
         auth: {
-           
-            user: 'td.gih4pc@gmail.com', // email
-            pass: 'gih4pcUps21' //password
-        },
-    }); 
+            type:'OAuth2',
+            user: 'td.gih4pc@gmail.com',
+            clientId: CLIENT_ID, 
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken: accessToken,
+        }
+    });
+
 
     //var mails = [`$"(listaEmails)"`]
     const mailOptions = {
