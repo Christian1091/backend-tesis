139f308fe241766e7ccadb2744cb45d2634a5fb4
ElyPriscy <puyaguaric@est.ups.edu.ec>
2022-02-16 17:34:18 -0500

    AreaD

--- controllers/area.controller.js
+++ controllers/area.controller.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const Area = require('../models/area.model');
+
 const getListAreas = async (req, res = response) => {
 
     const areas = await Area.find();
@@ -32,7 +33,42 @@ const crearArea = async (req, res = response) => {
     }
 }
 
+
+const eliminarArea = async (req, res = response) => {
+
+    const id = req.params.id;
+    try {
+
+        const area = await Area.findById(id);
+
+        if (!area) {
+
+            return res.status(404).json({
+                ok: true,
+                msg: 'No se ha eliminado correctamente'
+            });
+        }
+
+        await Area.findByIdAndDelete(id);
+
+        res.json({
+            ok: true,
+            msg: 'Area eliminada exitosamente'
+        })
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        })
+    }
+}
+
 module.exports = {
     getListAreas,
     crearArea,
+    eliminarArea,
 }
--- routes/area.js
+++ routes/area.js
@@ -4,9 +4,10 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-const {crearArea, getListAreas } = require('../controllers/area.controller');
+const {crearArea, getListAreas, eliminarArea } = require('../controllers/area.controller');
 router.get( '/areas', validarJWT, getListAreas );
 router.post( '/crear', validarJWT, crearArea);
+router.delete( '/eliminar/:id', validarJWT, eliminarArea);
 
 module.exports = router; 
 
